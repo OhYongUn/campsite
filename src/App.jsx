8c38f63d36b3885298945f665698d375349e9f0f
OhYongUn <dhdyddjs@naver.com>
2024-01-30 21:23:20 +0900

    Initial settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from "./styles/GlobalStyles.js";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import AppLayout from "./ui/AppLayout.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Login from "./pages/login.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <GlobalStyles />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} /> {/* 루트 경로를 로그인 페이지로 설정 */}
+
+                    {/* AppLayout을 적용할 경로들을 별도의 그룹으로 정의 */}
+                    <Route element={<AppLayout />}>
+                        <Route path="dashboard" element={<Dashboard />} />
+                        {/* 필요한 경우 여기에 추가 경로를 정의할 수 있습니다 */}
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
-// main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
