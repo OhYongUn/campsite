ae072205ac668e09e44389e7487a2f70144eca12
OhYongUn <dhdyddjs@naver.com>
2024-02-03 16:12:07 +0900

    Add signup page, ProtectedRoute and notification toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import GlobalStyles from "./styles/GlobalStyles.js";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter,  Route, Routes } from "react-router-dom";
 import AppLayout from "./ui/AppLayout.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Login from "./pages/login.jsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ProtectedRoute from "./ui/ProtectedRoute.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import {Toaster} from "react-hot-toast";
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            // staleTime: 60 * 1000,
-            staleTime: 0,
-        },
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
     },
+  },
 });
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
@@ -21,15 +25,39 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />{" "}
-          {/* 루트 경로를 로그인 페이지로 설정 */}
-          {/* AppLayout을 적용할 경로들을 별도의 그룹으로 정의 */}
-          <Route element={<AppLayout />}>
-            <Route path="dashboard" element={<Dashboard />} />
-            {/* 필요한 경우 여기에 추가 경로를 정의할 수 있습니다 */}
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="/dashboard" element={<Dashboard />} />
           </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp/>} />
         </Routes>
       </BrowserRouter>
+        <Toaster
+            position="top-center"
+            gutter={12}
+            containerStyle={{ margin: "8px" }}
+            toastOptions={{
+                success: {
+                    duration: 3000,
+                },
+                error: {
+                    duration: 5000,
+                },
+                style: {
+                    fontSize: "16px",
+                    maxWidth: "500px",
+                    padding: "16px 24px",
+                    backgroundColor: "var(--color-grey-0)",
+                    color: "var(--color-grey-700)",
+                },
+            }}
+        />
     </QueryClientProvider>
   );
 }
